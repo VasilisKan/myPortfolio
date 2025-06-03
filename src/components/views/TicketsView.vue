13a84a3312d67d671bf90b8610ebc2c41d2e2f73
Vasilis Kanellos <vasiliskanellos@Vasiliss-MacBook-Air.local>
2025-06-03 11:20:25 +0300

    adding views in dashboard

--- src/components/views/TicketsView.vue
+++ src/components/views/TicketsView.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useTicketStore } from './../../stores/ticketsStore'
 
 const ticketStore = useTicketStore()
+const selectedTicketId = ref<number | null>(null)
 
 onMounted(() => {
   ticketStore.loadOpenTickets()
 })
+
+const selectRow = (ticketId: number) => {
+  selectedTicketId.value = ticketId
+}
 </script>
 
 <template>
@@ -14,14 +19,33 @@ onMounted(() => {
     <div class="filters-wrapper">
       <h2>Filters</h2>
     </div>
-    <h1>All Tickets</h1>
     <div v-if="ticketStore.loading">Loading ...</div>
     <div v-else-if="ticketStore.error">{{ ticketStore.error }}</div>
-    <ul v-else>
-      <li v-for="ticket in ticketStore.tickets" :key="ticket.id">
-        <strong>{{ ticket.title }}</strong> - {{ ticket.status }}
-      </li>
-    </ul>
+    <table v-else class="tickets-table">
+      <thead>
+        <tr>
+          <th>A/A</th>
+          <th>Name</th>
+          <th>Title</th>
+          <th>Category</th>
+          <th>Created Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr 
+          v-for="(ticket, index) in ticketStore.tickets" 
+          :key="ticket.id" 
+          :class="{'selected-row': selectedTicketId === ticket.id}"
+          @click="selectRow(ticket.id)"
+        >
+          <td>{{ index + 1 }}</td> <!-- Auto-incrementing number -->
+          <td>{{ ticket.name }}</td>
+          <td>{{ ticket.title }}</td>
+          <td>{{ ticket.category }}</td>
+          <td>{{ ticket.createdDate }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <style scoped>
@@ -32,12 +56,42 @@ onMounted(() => {
     width: 100%;
     height: 100%;
 }
-.filters-wrapper{
+
+.filters-wrapper {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     width: 100%;
     height: 100px;
     background-color: #2e373e;
+    margin-bottom: 3em;
+}
+
+.tickets-table {
+    width: 95%;
+    margin: 0 auto;
+    border-collapse: collapse;
+    background-color: #2e373e;
+    color: white;
+}
+
+.tickets-table th,
+.tickets-table td {
+    padding: 12px 15px;
+    text-align: left;
+    border-bottom: 1px solid #444;
+}
+
+.tickets-table thead {
+    background-color: #1f2933;
+}
+
+.tickets-table tbody tr {
+    background-color: #2e373e;
+    cursor: pointer;
+}
+
+.tickets-table tbody tr.selected-row {
+    background-color: #4b565f !important; 
 }
 </style>
